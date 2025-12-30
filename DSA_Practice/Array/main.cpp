fa08a4139ce55b7ddb9f6f132cf36573bcc30c58
Amey Satwe <amey10051999@gmail.com>
2025-12-30 18:37:53 -0500

    More Array methods added. Adding Reverse using existing swap

--- DSA_Practice/Array/main.cpp
+++ DSA_Practice/Array/main.cpp
@@ -8,7 +8,13 @@ struct Array
 };
 struct Array arr = {{1, 2, 3, 4, 5}, 10, 5};
 Array *p = &arr;
-
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
 void Display(struct Array arr)
 {
     int i;
@@ -58,35 +64,140 @@ int Delete(struct Array *arr, int index)
     }
     return 0;
 }
-int linSearch(int key)
+int linSearch(Array arr, int key)
 {
-    for (int i = 0; i < p->length; i++)
+    for (int i = 0; i < arr.length; i++)
     {
-        if (p->A[i] == key)
+        if (arr.A[i] == key)
         {
             return i;
         }
     }
     return -1;
 }
+int bSearch(Array arr, int key)
+{
+    int l = 0, h = arr.length;
+    int mid;
 
+    while (l <= h)
+    {
+        mid = (l + h) / 2;
+        if (arr.A[mid] == key)
+        {
+            return mid;
+        }
+        else if (key < arr.A[mid])
+        {
+            h = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    return -1;
+}
+int get(struct Array arr, int index)
+{
+    if (index >= arr.length || index < 0)
+    {
+        return -1;
+    }
+    return arr.A[index];
+}
+void set(struct Array *arr, int index, int val)
+{
+    if (index >= arr->length || index < 0)
+    {
+        return;
+    }
+    arr->A[index] = val;
+}
+int max(struct Array arr)
+{
+    int max = arr.A[0];
+    for (int i = 0; i < arr.length; i++)
+    {
+        if (arr.A[i] > max)
+        {
+            max = arr.A[i];
+        }
+    }
+    return max;
+}
+int min(struct Array arr)
+{
+    int min = arr.A[0];
+    for (int i = 0; i < arr.length; i++)
+    {
+        if (arr.A[i] < min)
+        {
+            min = arr.A[i];
+        }
+    }
+    return min;
+}
+int sum(Array arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++)
+    {
+        // printf("%d", sum);
+        sum = sum + arr.A[i];
+    }
+    return sum;
+}
+float avg(Array arr)
+{
+    // printf("len %d\n", arr.length);
+    return (float)sum(arr) / arr.length;
+}
+void reverse(Array *arr)
+{
+    int l = 0;
+    int r = arr->length - 1;
+
+    while (l <= r)
+    {
+        swap(&arr->A[l], &arr->A[r]);
+        l++;
+        r--;
+    }
+    return;
+}
 int main()
 {
     Display(arr);
 
-    add(&arr, 6);
+    // add(&arr, 6);
+    // Display(arr);
+
+    // // insert(&arr, 0, 10);
+    // Display(arr);
+
+    // int x = Delete(&arr, 1);
+    // printf("Deleted %d from the array \n", x);
+    // Display(arr);
+
+    // int k = 10;
+    // int lin_search = linSearch(arr, k);
+    // printf("Found element %d at index %d \n", k, lin_search);
+
+    // int bK = 5;
+    // int bin_search = bSearch(arr, bK);
+    // printf("Found element %d at index %d \n", bK, bin_search);
+
+    // printf("Element at index 2 is %d \n", get(arr, 2));
+    // set(&arr, 2, 100);
+    // printf("Element at index 2 is %d \n", get(arr, 2));
+    // printf("Max element is %d \n", max(arr));
+    // printf("Min element is % d\n", min(arr));
+    // Display(arr);
+
+    // printf("Sum of array is %d\n", sum(arr));
+    // printf("Avg of array is %f\n", avg(arr));
+    reverse(&arr);
     Display(arr);
-
-    insert(&arr, 0, 10);
-    Display(arr);
-
-    int x = Delete(&arr, 1);
-    printf("Deleted %d from the array \n", x);
-    Display(arr);
-
-    int k = 10;
-    int lin_search = linSearch(k);
-    printf("Found element %d at index %d \n", k, lin_search);
-
     return 0;
 }
